e86caedf7edd2c2a87e11e1d44c9d35636c05aa8
xieruishen <xshen@olin.edu>
2020-09-21 11:52:28 -0400

    only left or right wall

--- warmup_project/scripts/wall_follow.py
+++ warmup_project/scripts/wall_follow.py
@@ -10,20 +10,35 @@ class WallFollowNode(object):
         self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.callback)
         self.velocity = Twist()
-        self.velocity.linear.x = 0.3
+        self.velocity.linear.x = 0.4
         self.velocity.angular.z = 0
-        self.k = 0.8
+        self.k = 0.15
 
 
 
     def callback(self, msg):
-        left_error = msg.ranges[45] - msg.ranges[135]
-        bottom_error = msg.ranges[135] - msg.ranges[45]
-        right_error = msg.ranges[135] - msg.ranges[315]
-        front_error = msg.ranges[45] - msg.ranges[315]
-        errors = [left_error, bottom_error, right_error, front_error]
-        min_error = min(errors)
-        self.velocity.angular.z = self.k*min_error
+        left_error =  msg.ranges[45] - msg.ranges[135]
+        print("left error: %s" %left_error)
+        #back_error = msg.ranges[135] - msg.ranges[225]
+        right_error = msg.ranges[225] - msg.ranges[315]
+        print("right error: %s" %right_error)
+        #front_error = msg.ranges[45] - msg.ranges[315]
+        #errors = [left_error, back_error, right_error, front_error]
+        errors = [left_error, right_error]
+
+        # Find the side with minimum difference
+        errors_absolute =  [abs(error) for error in errors]
+        errors_absolute_min = min(errors_absolute)
+        error_absolute_min_index = errors_absolute.index(errors_absolute_min)
+        min_error = errors[error_absolute_min_index]
+        
+        print("min_error: %s" % min_error)
+        side_str = ["left", "right"]
+        print("side: %s" %side_str[error_absolute_min_index])
+        if (abs(min_error) < 0.5e-10):
+            self.velocity.angular.z = 0
+        else:
+            self.velocity.angular.z = self.k*min_error
 
     def run(self):
         r = rospy.Rate(2)
